8d0d317478f0b6cc7465beffd3206fa6a8f7860a
pierrejava <pierrejava@gmail.com>
2025-09-01 14:11:15 +0200

    Ajoutez des fichiers projet.

---
+++ Domain/HouseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class HouseModel
+    {
+        public int HouseId { get; set; }
+        public int HouseIP4 { get; set; }
+        public int HouseOwner_Id { get; set; }
+        public bool IsHouseActive { get; set; }
+
+
+        public string HouseName { get; set; }
+        public string HousePassword { get; set; }
+    }
+
+}
+
---
+++ Domain/MicroIot.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class MicroIotDevice
+    {
+        public int DeviceIotId { get; set; }
+        
+        public int DeviceOwnerId { get; set; }
+        public int DeviceNumericValue { get; set; }
+        public bool DeviceAnalogicValue { get; set; }
+
+        public int DeviceCategoryId { get; set; }
+
+        public string DeviceName { get; set; } = string.Empty;
+
+
+    }
+}
